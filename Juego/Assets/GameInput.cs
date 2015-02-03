d8016a4869e63d563b50615175532c5790b09805
Ramón Serrano López <rslnautic@gmail.com>
2015-02-03 17:20:28 +0100

    first commit
    first commit

---
+++ Juego/Assets/GameInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInput : MonoBehaviour {
+		
+	public static float xMovementP1;
+	public static bool jumpP1;
+	public static float xAimP1;
+	public static float yAimP1;
+
+
+	readonly KeyCode leftP1 = KeyCode.A;
+	readonly KeyCode rightP1 = KeyCode.D;
+	readonly KeyCode jumpsP1 = KeyCode.W;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (leftP1) && Input.GetKey (rightP1)) {
+			xMovementP1 = 0;
+		} else if (Input.GetKey (leftP1)) {
+			xMovementP1 = -1;
+		} else if (Input.GetKey (rightP1)) {
+			xMovementP1 = 1;
+		} else {
+			xMovementP1 = 0;
+		}
+	}
+}
---
+++ Juego/Assets/Personaje.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Personaje : MonoBehaviour {
+
+	public float maxSpeed = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void FixedUpdate() {
+		rigidbody2D.velocity = GameInput.xMovementP1 * Vector2.right * maxSpeed;
+	}
+}
