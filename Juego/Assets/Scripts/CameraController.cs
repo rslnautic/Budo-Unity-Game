0dcb28c79fcea1fa610769ad989e4ac3a8137bdc
Ramón Serrano López <rslnautic@gmail.com>
2015-03-02 16:58:32 +0100

    camera
    camera

---
+++ Juego/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject p1;
+	public GameObject p2;
+	
+	private Vector3 offset;
+	private Vector3 newOffset;
+
+	public float interpolationSt = 3;
+	public float verticalOffset = 5;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float media;
+		if (!p1.GetComponent<Personaje> ().dying && !p2.GetComponent<Personaje> ().dying) {
+			media = (p1.transform.position.y + p2.transform.position.y) / 2;
+			newOffset = Vector3.Lerp (transform.position, new Vector3(offset.x, media + verticalOffset, offset.z), Time.deltaTime*interpolationSt);
+			transform.position = newOffset;
+		}
+
+	}
+}
--- Juego/Assets/Scripts/Personaje.cs
+++ Juego/Assets/Scripts/Personaje.cs
@@ -23,9 +23,14 @@ public class Personaje : MonoBehaviour {
 		}
 	}*/
 
+	public float startingY;
+	float timer = 0;
+	public float deathSpeed = 1;
+	public AnimationCurve deathCurve;
+
 	// Use this for initialization
 	void Start () {
-	
+		startingY = transform.localPosition.y;
 	}
 	// Update is called once per frame
 	enum MoveState {HELD,JUMPING,FALLING,WALL}
@@ -79,140 +84,142 @@ public class Personaje : MonoBehaviour {
 	void FixedUpdate() {
 
 		setLayer ();
-		SetLookingDirection ();
 		checkLife ();
-		SetArmRotation ();
-
-		//static RaycastHit2D Raycast(Vector2 origin, Vector2 direction, float distance = Mathf.Infinity, int layerMask = DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity);
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .55f, groundLayers);
-		if (hit.collider != null) {
+		if (!dying) {
+			SetLookingDirection ();
+			SetArmRotation ();
+			
+			//static RaycastHit2D Raycast(Vector2 origin, Vector2 direction, float distance = Mathf.Infinity, int layerMask = DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity);
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, .55f, groundLayers);
+			if (hit.collider != null) {
 				moveState = MoveState.HELD;
-			fallHoldTimer = 0;
-		} else {
-			if(moveState == MoveState.HELD){
-				fallHoldTimer += Time.deltaTime;
-				if(fallHoldTimer > fallHoldTime)moveState = MoveState.FALLING;
-			}
-		}
-
-		if(GameInput.GetPlayerJump(charact) && moveState == MoveState.HELD){
-			jumpTimer = 0;
-			moveState = MoveState.JUMPING;
-		}
-		float factor = 0;
-		//moveState es el estado en el que esta el jugador, puede estar en el suelo(HELD), saltando(JUMPING),
-		//o cayendo(FALLING)
-		Vector2 verticalMovement = Vector2.zero;
-
-		switch (moveState) {
-		case MoveState.HELD:
-			factor = holdingFactor;
-			verticalMovement = Vector2.up*.05f;
-			break;
-		case MoveState.JUMPING:
-			
-			factor = jumpingFactor;
-			jumpTimer += Time.deltaTime;
-			float normalizedJumpTime = jumpTimer / jumpTime;
-			float jumpSpeedInTime = jumpSpeedCurve.Evaluate (normalizedJumpTime) * jumpSpeed;
-			verticalMovement = -Vector2.up * jumpSpeedInTime;
-			if (normalizedJumpTime >= 1) {
-				moveState = MoveState.FALLING;
-				fallTimer = 0;
-			}
-			if(normalizedJumpTime > 0.3 && !GameInput.GetPlayerJump(charact)){
-				moveState = MoveState.FALLING;
-				fallTimer = 0;
-			}
-			break;
-		case MoveState.FALLING:
-			
-			factor = fallingFactor;
-			if(fallTimer < 1.2)
-				fallTimer += Time.deltaTime;
-			//float normalizedFallTime = fallTimer / maxFallSpeed;
-			float fallSpeedInTime = fallSpeedCurve.Evaluate (fallTimer) * fallSpeed;
-			verticalMovement = Vector2.up * fallSpeedInTime;
-			
-			RaycastHit2D hitL = Physics2D.Raycast(transform.position, -Vector2.right, .55f, groundLayers);
-			RaycastHit2D hitR = Physics2D.Raycast(transform.position, Vector2.right, .55f, groundLayers);
-			if (hitL.collider != null || hitR.collider != null) {
-				moveState = MoveState.WALL;
-			}
-			break;
-		case MoveState.WALL:
-			factor = fallingFactor;
-			if(fallTimer < 1.2)
-				fallTimer += Time.deltaTime;
-
-			float fallSpeedInTime2 = fallSpeedCurve.Evaluate (fallTimer) * fallSpeed;
-			verticalMovement = Vector2.up* 0.2f * fallSpeedInTime2;
-			//visualCharacter.localEulerAngles = new Vector3 (0, -180, 0);
-
-			RaycastHit2D hitL2 = Physics2D.Raycast(transform.position, -Vector2.right, .55f, groundLayers);
-			RaycastHit2D hitR2 = Physics2D.Raycast(transform.position, Vector2.right, .55f, groundLayers);
-			if (hitL2.collider == null && hitR2.collider == null) {
-				moveState = MoveState.FALLING;
-			}
-			if(hitL2.collider != null){
-				visualCharacter.localEulerAngles = new Vector3 (8, 0, 0);
-			}
-			else if(hitR2.collider != null){
-				visualCharacter.localEulerAngles = new Vector3 (0, 180, 0);
-			}
-
-			wallJumpingRight = true;
-
-			if(hitR2.collider != null)
-				wallJumpingRight = false;
-
-			if(GameInput.GetPlayerJump(charact) && !jumpedWall){
-				//jumpedWall = true;
-				jumpTimer = 0;
-				moveState = MoveState.JUMPING;
-				if(wallJumpingRight){
-					xlateralcurveposition = 0.9f;
-				}else{
-					xlateralcurveposition = 0.1f;
+				fallHoldTimer = 0;
+			} else {
+				if(moveState == MoveState.HELD){
+					fallHoldTimer += Time.deltaTime;
+					if(fallHoldTimer > fallHoldTime)moveState = MoveState.FALLING;
 				}
 			}
-			break;
-		}
-		
-		float direction = 0;
-		bool frenando = false;
-		if (GameInput.GetPlayerXMovement(charact) > 0.1f || GameInput.GetPlayerXMovement(charact) < -0.1f) {
-			direction = GameInput.GetPlayerXMovement(charact);
-			if(direction>0 && xlateralcurveposition >0.5f || direction < 0 && xlateralcurveposition <0.5f){
-				direction*=aceleracion;
+			
+			if(GameInput.GetPlayerJump(charact) && moveState == MoveState.HELD){
+				jumpTimer = 0;
+				moveState = MoveState.JUMPING;
+			}
+			float factor = 0;
+			//moveState es el estado en el que esta el jugador, puede estar en el suelo(HELD), saltando(JUMPING),
+			//o cayendo(FALLING)
+			Vector2 verticalMovement = Vector2.zero;
+			
+			switch (moveState) {
+			case MoveState.HELD:
+				factor = holdingFactor;
+				verticalMovement = Vector2.up*.05f;
+				break;
+			case MoveState.JUMPING:
+				
+				factor = jumpingFactor;
+				jumpTimer += Time.deltaTime;
+				float normalizedJumpTime = jumpTimer / jumpTime;
+				float jumpSpeedInTime = jumpSpeedCurve.Evaluate (normalizedJumpTime) * jumpSpeed;
+				verticalMovement = -Vector2.up * jumpSpeedInTime;
+				if (normalizedJumpTime >= 1) {
+					moveState = MoveState.FALLING;
+					fallTimer = 0;
+				}
+				if(normalizedJumpTime > 0.3 && !GameInput.GetPlayerJump(charact)){
+					moveState = MoveState.FALLING;
+					fallTimer = 0;
+				}
+				break;
+			case MoveState.FALLING:
+				
+				factor = fallingFactor;
+				if(fallTimer < 1.2)
+					fallTimer += Time.deltaTime;
+				//float normalizedFallTime = fallTimer / maxFallSpeed;
+				float fallSpeedInTime = fallSpeedCurve.Evaluate (fallTimer) * fallSpeed;
+				verticalMovement = Vector2.up * fallSpeedInTime;
+				
+				RaycastHit2D hitL = Physics2D.Raycast(transform.position, -Vector2.right, .55f, groundLayers);
+				RaycastHit2D hitR = Physics2D.Raycast(transform.position, Vector2.right, .55f, groundLayers);
+				if (hitL.collider != null || hitR.collider != null) {
+					moveState = MoveState.WALL;
+				}
+				break;
+			case MoveState.WALL:
+				factor = fallingFactor;
+				if(fallTimer < 1.2)
+					fallTimer += Time.deltaTime;
+				
+				float fallSpeedInTime2 = fallSpeedCurve.Evaluate (fallTimer) * fallSpeed;
+				verticalMovement = Vector2.up* 0.2f * fallSpeedInTime2;
+				//visualCharacter.localEulerAngles = new Vector3 (0, -180, 0);
+				
+				RaycastHit2D hitL2 = Physics2D.Raycast(transform.position, -Vector2.right, .55f, groundLayers);
+				RaycastHit2D hitR2 = Physics2D.Raycast(transform.position, Vector2.right, .55f, groundLayers);
+				if (hitL2.collider == null && hitR2.collider == null) {
+					moveState = MoveState.FALLING;
+				}
+				if(hitL2.collider != null){
+					visualCharacter.localEulerAngles = new Vector3 (8, 0, 0);
+				}
+				else if(hitR2.collider != null){
+					visualCharacter.localEulerAngles = new Vector3 (0, 180, 0);
+				}
+				
+				wallJumpingRight = true;
+				
+				if(hitR2.collider != null)
+					wallJumpingRight = false;
+				
+				if(GameInput.GetPlayerJump(charact) && !jumpedWall){
+					//jumpedWall = true;
+					jumpTimer = 0;
+					moveState = MoveState.JUMPING;
+					if(wallJumpingRight){
+						xlateralcurveposition = 0.9f;
+					}else{
+						xlateralcurveposition = 0.1f;
+					}
+				}
+				break;
+			}
+			
+			float direction = 0;
+			bool frenando = false;
+			if (GameInput.GetPlayerXMovement(charact) > 0.1f || GameInput.GetPlayerXMovement(charact) < -0.1f) {
+				direction = GameInput.GetPlayerXMovement(charact);
+				if(direction>0 && xlateralcurveposition >0.5f || direction < 0 && xlateralcurveposition <0.5f){
+					direction*=aceleracion;
+				} else {
+					direction*=aceleracionInversa;
+				}
 			} else {
-				direction*=aceleracionInversa;
+				frenando = true;
+				if(xlateralcurveposition > 0.5f)
+				{
+					direction =-frenado;
+				} else {
+					direction =frenado;
+				}
 			}
-		} else {
-			frenando = true;
-			if(xlateralcurveposition > 0.5f)
-			{
-				direction =-frenado;
-			} else {
-				direction =frenado;
-			}
+			// Limita los valores minimos y maximos de una curva	
+			xlateralcurveposition += direction * Time.deltaTime * factor;
+			
+			
+			if (frenando) {
+				if(xlateralcurveposition > 0.5f && direction >0 || xlateralcurveposition < 0.5f && direction <0)
+				{
+					xlateralcurveposition = 0.5f;
+				}
+			}	
+			xlateralcurveposition = Mathf.Clamp (xlateralcurveposition,0,1);
+			if(weapon != null)
+				weapon.CheckShooting ();
+			
+			float lateralspeed = XCurve.Evaluate (xlateralcurveposition);
+			rigidbody2D.velocity = (Vector2.right * maxSpeed * lateralspeed) - verticalMovement;
 		}
-		// Limita los valores minimos y maximos de una curva	
-		xlateralcurveposition += direction * Time.deltaTime * factor;
-		
-
-		if (frenando) {
-			if(xlateralcurveposition > 0.5f && direction >0 || xlateralcurveposition < 0.5f && direction <0)
-			{
-				xlateralcurveposition = 0.5f;
-			}
-		}	
-		xlateralcurveposition = Mathf.Clamp (xlateralcurveposition,0,1);
-		if(weapon != null)
-		weapon.CheckShooting ();
-
-		float lateralspeed = XCurve.Evaluate (xlateralcurveposition);
-		rigidbody2D.velocity = (Vector2.right * maxSpeed * lateralspeed) - verticalMovement;
 	}
 
 	
@@ -241,11 +248,16 @@ public class Personaje : MonoBehaviour {
 		}
 	}
 
+	public bool dying = false;
+
 	void checkLife(){
-		if (vida <= 0) 
-		{
+		if (vida <= 0 && !dying) {
 			vida = 0;
-			Destroy (this.gameObject);
+			Die ();
+			//Destroy (this.gameObject);
+		} else if(dying){
+			timer += Time.deltaTime * deathSpeed;
+			transform.localPosition = new Vector3 (transform.localPosition.x, startingY + deathCurve.Evaluate (timer)*deathDistance, transform.localPosition.z);
 		}
 		switch (charact) {
 		case Personaje.Pjs.PJ1:
@@ -275,6 +287,17 @@ public class Personaje : MonoBehaviour {
 			break;
 		}
 		
+	}
+
+	public float deathDistance = 50;
+
+	public void Die() {
+			transform.parent = null;
+			collider2D.enabled = false;
+		    dying = true;
+			timer = 0;
+			startingY = transform.localPosition.y;
+			//transform.localScale = new Vector3 (1, scaleDeathCurve.Evaluate (timer), 1);
 	}
 
 	void setLayer() {
