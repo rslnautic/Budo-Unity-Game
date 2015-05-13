36687342bf0d6f9d3928adf16c1ab7a575545460
Ramón Serrano López <rslnautic@gmail.com>
2015-05-13 16:21:39 +0200

    sonido

---
+++ Juego/Assets/MusicController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (Fade.rondasGanadasP1>0 | Fade.rondasGanadasP2>0) {
+			Debug.Log("no es la primera partida");
+			Destroy(this.gameObject);
+
+		} else {
+			Debug.Log("Primera partida");
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
